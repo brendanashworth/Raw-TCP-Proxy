f2cd23e61389cc6522f431071bb03ee5633d8aaf
boboman13 <rich@safe-node.com>
2014-06-16 15:40:25 -0700

    Make this thing beautiful

--- bin/proxy.pl
+++ bin/proxy.pl
@@ -7,15 +7,33 @@
 use strict;
 use warnings;
 
-print("Compiling proxy... \n");
+# Colors
+my $primary = "\e[36m";
+my $secondary = "\e[33m";
+my $fail = "\e[31m";
+my $reset = "\e[0m";
+
+# Before we do anything, check if they provided all the arguments.
+unless (defined $ARGV[1]) {
+	print($primary); # color
+	print("This is the Raw TCP Proxy wrapper.\n");
+	print("Example usage: \n");
+	print($secondary . "  ./bin/proxy.pl 0.0.0.0:1357 127.0.0.1:1358\n");
+	print($primary   . "  First argument is the listening IP / port\n");
+	print(             "  Second argument is the output IP / port.\n");
+	print("Use " . $secondary . "./bin/proxy.pl" . $primary . " again to access this help." . $reset . "\n");
+	exit;
+}
+
+print($primary . "Compiling proxy... \n" . $reset);
 
 `mvn package`;
 # Did it compile fine?
 if ( $? == -1 ) {
-	print("Compiling failed. \n");
+	print($fail . "Compiling failed. \n" . $reset);
 	exit;
 } else {
-	print("Compiled. ");
+	print($secondary . "Compiled. " . $reset);
 }
 
 # Get the host, listening ports, output host etc.
@@ -29,7 +47,7 @@ my $outputhost = $output[0];
 my $outputport = $output[1];
 
 # Boot.
-print("Booting... \n");
+print($primary . "Booting... \n" . $reset);
 
 my @args = ('java', '-jar', 'target/Raw-TCP-Proxy-1.0-SNAPSHOT.jar', '-h', $listenhost, '-l', $listenport, '-o', $outputhost, '-p', $outputport);
 
